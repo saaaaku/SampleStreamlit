0cf2587974037fc954d06e973b0ad16421c9f45b
Yusaku Nakaue <yn.1109@gmail.com>
2021-01-10 14:33:23 +0900

    test

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ st.title('Streamlit')
 """
 ## プログレスバー
 """
-latest_iteration = st.empty()
-bar = st.progress(0)
+# latest_iteration = st.empty()
+# bar = st.progress(0)
 
-for i in range(100):
-    latest_iteration.text(f'Iteration {i+1}')
-    bar.progress(i+1)
-    time.sleep(0.01)
+# for i in range(100):
+#     latest_iteration.text(f'Iteration {i+1}')
+#     bar.progress(i+1)
+#     time.sleep(0.1)
 
 
 """
